f9c7c32998f3a209a53575f00c22b0d2631975ea
Masao-Taketani <taket003@umn.edu>
2021-01-18 01:01:07 +0900

    start implementing the training script

---
+++ train.py
@@ -0,0 +1,30 @@
+from absl import app
+from absl import flags
+
+import tensorflow as tf
+
+from data_loader import get_data
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string("attr_path", 
+                    "data/celeba/list_attr_celeba.txt", 
+                    "path to the attribute label")
+flags.DEFINE_list("selected_attrs",
+                  "Black_Hair, Blond_Hair, Brown_Hair, Male, Young",
+                  "attributes for training")
+
+
+def main(argv):
+    train_imgs, train_lbls, test_imgs, test_lbls = get_data(FLAGS.attr_path, 
+                                                            FLAGS.selected_attrs)
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_imgs, train_lbls))
+    for img, lbl in train_dataset.take(5):
+        print(img, lbl)
+
+
+
+if __name__ == "__main__":
+    app.run(main)
--- utils.py
+++ utils.py
@@ -25,3 +25,25 @@ def normalize(img):
     img = tf.cast(img, tf.float32)
     img = (img / 127.5) - 1.0
     return img
+
+
+def random_horizontal_flip(img):
+    if tf.random.uniform(()) > 0.5:
+        img = tf.image.flip_left_right(img)
+    return img
+
+
+def resize(img, size=128):
+    img = tf.image.resize(img, 
+                          [size, size], 
+                          method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    return img
+
+
+@tf.function
+def preprocess(img, center_crop_size=178, resize=128):
+    img = random_horizontal_flip(img)
+    img = center_crop(img, center_crop_size)
+    img = resize(img, resize)
+    img = normalize(img)
+    return img
