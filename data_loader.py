060edeb301305c7226e9baef9242db0c0d24c371
Masao-Taketani <taket003@umn.edu>
2021-01-21 02:22:10 +0900

    understand 'create_labels' func and it is done

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import random
 import os
+import numpy as np
 
 
 IMG_DIR = "data/celeba/images/"
@@ -25,6 +26,7 @@ def get_data(attr_path, selected_attrs):
     lines = lines[2:]
     random.seed(1234)
     random.shuffle(lines)
+
     for i, line in enumerate(lines):
         split = line.split()
         filename = split[0]
@@ -33,7 +35,10 @@ def get_data(attr_path, selected_attrs):
         label = []
         for attr_name in selected_attrs:
             idx = attr2idx[attr_name]
-            label.append(values[idx] == "1")
+            if values[idx] == "1":
+                label.append(1)
+            else:
+                label.append(0)
             
         if (i+1) < 2000:
             test_imgs.append(os.path.join(IMG_DIR, filename))
@@ -50,6 +55,33 @@ def get_data(attr_path, selected_attrs):
     return train_imgs, train_lbls, test_imgs, test_lbls
 
 
+def create_labels(c_org, c_dim=5, selected_attrs=None):
+    """Generate each target label from a original label"""
+    # Get hair colo indices
+    c_org = np.array(c_org)
+    hair_color_indices = []
+    for i, attr_name in enumerate(selected_attrs):
+        if attr_name in ["Black_Hair", "Blond_Hair", "Brown_Hair", "Gray_Hair"]:
+            hair_color_indices.append(i)
+
+    c_trg_list = []
+    for i in range(c_dim):
+        c_trg = c_org.copy()
+        # Set one hair color to 1 and the rest to 0.
+        if i in hair_color_indices:
+            c_trg[:, i] = 1
+            for j in hair_color_indices:
+                if j != i:
+                    c_trg[:, j] = 0
+        else:
+            # Reverse each attribute for non-hair-color class
+            c_trg[:, i] = (c_trg[:, i] == 0)
+
+        c_trg_list.append(c_trg)
+    
+    return c_trg_list
+
+
 if __name__ == "__main__":
     attr_path = "data/celeba/list_attr_celeba.txt"
     selected_attrs = ['Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Male', 'Young']
@@ -59,5 +91,11 @@ if __name__ == "__main__":
     for img, lbl in dataset.take(5):
         print(img, lbl)
 
+    print("shape of train_lbls:", len(train_lbls), len(train_lbls[0]))
+
+    orgs_to_test = train_lbls[:5]
+    print("original test labels\n", orgs_to_test)
+    trgs_to_test = create_labels(orgs_to_test, 5, selected_attrs)
+    print("target test labels\n", trgs_to_test)
 
     print("Finished testing the CelebA dataset!")
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from absl import flags
 import tensorflow as tf
 
 from data_loader import get_data
+from utils import *
 
 
 FLAGS = flags.FLAGS
@@ -15,15 +16,22 @@ flags.DEFINE_list("selected_attrs",
                   "Black_Hair, Blond_Hair, Brown_Hair, Male, Young",
                   "attributes for training")
 flags.DEFINE_integer("c_dim", 5, "dimension of domain labels")
+flags.DEFINE_integer("batch_size", 16, "mini-batch size")
 
 
 def main(argv):
     train_imgs, train_lbls, test_imgs, test_lbls = get_data(FLAGS.attr_path, 
                                                             FLAGS.selected_attrs)
-
+    # prepare for training dataset
     train_dataset = tf.data.Dataset.from_tensor_slices((train_imgs, train_lbls))
+    train_dataset = tf.data.Dataset.map()
     for img, lbl in train_dataset.take(5):
         print(img, lbl)
+
+    # Get fixed inputs for debugging
+    label_org
+    label_trg = tf.random.shuffle(label_org)
+    targ_label = orig_label[rand_idxes]
 
     gen, disc = model.build_model(FLAGS.c_dim)
 
